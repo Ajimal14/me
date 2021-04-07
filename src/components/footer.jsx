69995b70c561cf2c6c7a24c124b1a474fa91d200
Ashish Ajimal <ashishajimal14@gmail.com>
2021-04-07 20:04:56 +0530

    added build

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 100px;
+    background: ${props => props.theme.headingColor};
+    margin-top: -50px;
+    @media only screen and (max-width: 480px){
+        height: 70px;
+        margin-top: -20px;
+    }
+`;
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <Container>
+            
+        </Container>
     )
 }
 
